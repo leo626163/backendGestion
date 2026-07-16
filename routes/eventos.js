38a5b2086ddf5e658509639dc5611fff202a4639
leo626163 <leo626163@gmail.com>
2026-07-16 14:20:39 -0400

    fix user student

--- routes/eventos.js
+++ routes/eventos.js
@@ -41,7 +41,7 @@ router.get('/rechazados',protect, getEventosRechazados);
 //router.get('/details/:id', getEventDetailsById);
 router.get('/mis-inscripciones', protect, getMisInscripciones);
 router.post('/:id/registrar', protect, registrarEventoEstudiante);
-router.get('/estudiantes-inscritos-facultad',protect, estudiantesInscritosEnEvento);
+router.get('/estudiantesInscritos',protect, estudiantesInscritosEnEvento);
 
 //router.get('/listar-pendientes', pendientes); // si necesitas esta ruta
 //router.get('/pendientes',protect, getEventosPendientesPorArea);
